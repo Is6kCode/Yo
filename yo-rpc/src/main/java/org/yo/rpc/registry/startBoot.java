3f0505ff0a8170430d7fab1add4c6564c00463d7
Is6kCode <759056164@qq.com>
2014-08-02 18:10:57 +0800

    MAVEN编译配置

--- yo-rpc/src/main/java/org/yo/rpc/registry/startBoot.java
+++ yo-rpc/src/main/java/org/yo/rpc/registry/startBoot.java
@@ -1,13 +1,14 @@
 package org.yo.rpc.registry;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class startBoot {
 
 	    public static void main(String[] args) throws Exception {
 	        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] 
 	        		   {"classpath:applicationProvider.xml"});
+	        
 	        context.start();
+	        System.out.println("输入任意字符终止程序");
 	        System.in.read(); // 按任意键退出
 	    }
 
